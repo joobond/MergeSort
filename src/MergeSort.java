95dccb6b03b5d61eb4786b751014c16492c81671
João Bond <jobond46@gmail.com>
2018-11-13 19:29:51 -0400

    Comentando o código

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,9 +1,12 @@
 public class MergeSort {
 
     public static void main(String[] args) {
+
+        //Definição do array à ser ordenado
         int x[] = {3,4,1,2,5,9,6,7,8,0};
         int i;
 
+        //Mostrar o array pulando linhas
         for(i=0; i<=9;i++){
             System.out.println((i+1)+"º número: "+x[i]);
         }
@@ -11,8 +14,11 @@ public class MergeSort {
         System.out.println("-----------------------------");
         System.out.println("Agora o vetor ordenado: ");
 
+        //Chamando o algoritmo para ordenar o array;
+        //Passo dois parâmetros, da posição 0 à posição 9 (10 elementos)
         merge(x,0,9); //Declara qual é o inicio e qual e o fim
 
+        //Mostrando o array ordenado
         for(i=0; i<=9;i++){
             System.out.println((i+1)+"º número: "+x[i]);
         }
@@ -22,22 +28,30 @@ public class MergeSort {
     //Depois do meio+1 ao fim e só ai que eu intercalo esses dois;
 
     public static void merge(int x[], int inicio, int fim){
-        int meio;
+        int meio; //Variável para guardar o meio
+
+        //Se à posição do fim for maior que à do inicio, entra aqui
         if(inicio < fim){
-            meio = (inicio + fim)/2;
-            merge(x, inicio, meio);
-            merge(x, meio+1, fim);
-            intercala(x, inicio, fim, meio);
+            meio = (inicio + fim)/2; //Calculo do meio
+            merge(x, inicio, meio); //Recursividade, chama o algoritmo e executa do
+                                    //inicio ao meio
+            merge(x, meio+1, fim); //Recursividade, chama o algoritmo de uma posição após meio
+                                        //até o fim
+            intercala(x, inicio, fim, meio);//Chama o algoritmo para intercalar os dois vetores
         }
     }
 
+    //Aqui eu passo  o vetor que quero intercalar, o inicio, o fim e o meio do array
     public static void intercala(int x[], int inicio, int fim, int meio){
-        int poslivre, inicio_vetor1, inicio_vetor2, i;
+        int poslivre, inicio_vetor1, inicio_vetor2, i; //Definição das variaveis
         int aux[] = new int[10]; //Criando um vetor auxliar;
-        inicio_vetor1 = inicio;
-        inicio_vetor2 = meio+1;
-        poslivre = inicio;
+        inicio_vetor1 = inicio; //Adicionando as posições dos parametros nas variaveis
+        inicio_vetor2 = meio+1; //Adicionando as posições dos parametros nas variaveis
+        poslivre = inicio; //Adicionando as posições dos parametros nas variaveis
 
+        //Enquanto o inicio do vetor um for menor ou igual ao meio
+        // e inicio do vetor dois for menor ou igual ao fim
+        // entra no laço
         while(inicio_vetor1 <= meio && inicio_vetor2 <= fim){
             if(x[inicio_vetor1] <= x[inicio_vetor2]){ //Compara o inicio do primeiro com o inicio do segundo
                 aux[poslivre] = x[inicio_vetor1];
@@ -49,16 +63,22 @@ public class MergeSort {
             poslivre = poslivre+1;
         }
 
+        //Se ainda tiver algum elemento do inicio ao meio que não passou no laço
+        // entra aqui
         for(i=inicio_vetor1; i<=meio; i++){
             aux[poslivre] = x[i];
             poslivre = poslivre+1;
         }
 
+        //Se ainda tiver algum elemento do meio ao fim que não passou no laço
+        // entra aqui
         for(i = inicio_vetor2; i<=fim; i++){
             aux[poslivre] = x[i];
             poslivre = poslivre+1;
         }
 
+        //Se ainda tiver algum elemento do inicio ao fim que não passou no laço
+        // entra aqui
         for (i = inicio; i<=fim;i++){
             x[i] = aux[i];
         }
